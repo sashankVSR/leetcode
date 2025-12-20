985bd6d77af34e5edf8c3dbd54b8f6cdb587772e
Sriram Sashank Veluvali <sashank6446@gmail.com>
2025-12-20 14:58:34 +0530

    Time: 719 ms (0.07%) | Memory: 7.7 MB (86.70%) - LeetSync

--- 292-nim-game/nim-game.cpp
+++ 292-nim-game/nim-game.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-        if(n % 4 == 0){
+        if(n <= 3){
+            return true;
+        }
+        if(n == 4){
             return false;
+        }
+        while(n > 0){
+            n -= 3;
+            if(n == 1) {
+                return false;
+            }
+            n--;
         }
         return true;
     }
