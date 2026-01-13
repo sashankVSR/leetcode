5caae14e75f260e151347fb9e1d38cd0cfcbb31b
Sriram Sashank Veluvali <sashank6446@gmail.com>
2026-01-13 09:21:20 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (7.95%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -6,24 +6,15 @@ public:
         while(low <= high){
             int mid = low + (high-low)/2;
 
-            if (nums[mid] == target){
+            if(nums[mid] == target){
                 return mid;
             }
 
-            if(nums[low] <= nums[mid]){  
-                if (target >= nums[low] && target < nums[mid])
-                    high = mid-1;
-                else 
-                    low = mid+1;
-            }
+            if(nums[mid] < target)
+                low = mid+1;
 
-            else{
-                if(target > nums[mid] && target <= nums[high])
-                    low = mid+1;
-                
-                else 
-                    high = mid-1;
-            }
+            else 
+                high = mid-1;
         }
         return -1;
     }
