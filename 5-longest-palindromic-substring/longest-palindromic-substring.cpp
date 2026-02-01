291f20c6a10a4cb527159ca2ea90012d03a8a89a
Sriram Sashank Veluvali <sashank6446@gmail.com>
2026-02-01 20:35:30 +0530

    Time: 7 ms (86.33%) | Memory: 9.3 MB (78.92%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -19,7 +19,7 @@ public:
 
         int len = max(len1, len2);
 
-        if(len > end -start){
+        if(len > end - start){
             start = i - (len - 1)/2;
             end = i + (len/2);
         }
