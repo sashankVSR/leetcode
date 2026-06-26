a5369f53266995365106eeb81d22d396c5e19228
Sriram Sashank Veluvali <sashank6446@gmail.com>
2026-06-26 21:28:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (78.93%) - LeetSync

---
+++ 390-elimination-game/elimination-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastRemaining(int n) {
+        long long head = 1;
+        long long step = 1;
+        long long remaining = n;
+        bool leftToRight = true;
+
+        while (remaining > 1) {
+            if (leftToRight || remaining % 2 == 1)
+                head += step;
+
+            remaining /= 2;
+            step *= 2;
+            leftToRight = !leftToRight;
+        }
+        return (int)head;
+    }
+};
